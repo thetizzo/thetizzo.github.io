eeb576f4fd6a0b5709cf89f5f4760a544fc73b35
thetizzo <j.m.taylor1@gmail.com>
2017-08-18 23:46:59 -0600

    wip

--- _includes/resume/resume.js
+++ _includes/resume/resume.js
@@ -1,3 +1,5 @@
+"use strict";
+
 $(document).ready(function() {
   $('.collapse').on('shown.bs.collapse', function (e) {
     clicked = $(document).find("[data-target='#" + $(e.target).attr('id') + "']").children().first();
@@ -9,5 +11,11 @@ $(document).ready(function() {
     clicked = $(document).find("[data-target='#" + $(e.target).attr('id') + "']").children().first();
     clicked.removeClass("fa-angle-double-up");
     clicked.addClass("fa-angle-double-down");
+  });
+
+  $('#download-pdf').on('click', function(e) {
+    e.preventDefault();
+    var docDefinition = {content: $('#resumeJson').text()};
+    pdfMake.createPdf(docDefinition).open();
   });
 });

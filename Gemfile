4201b3c0296839b343f7a326b06c652a4a65affc
thetizzo <jason.taylor@hey.com>
2022-01-14 00:19:06 -0700

    Added Tailwind CSS and removed existing styles

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ gem 'jekyll', '~> 4.2'
 group :jekyll_plugins do
   gem 'jekyll-timeago', '~> 0.14'
   gem 'jekyll-gist', '~> 1.5'
+  gem 'jekyll-postcss', '~> 0.5'
 end
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
+}
